6b4572e74603489509a18c573625154c4be4d469
DalekStarlord <gabreylabadies3@gmail.com>
2025-11-09 21:47:15 +0800

    minor fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-eb583a20'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qpb2dsee59o"
+    "revision": "0.a6k56sil398"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
